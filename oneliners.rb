a1d33026c04dda921ac5a6046530701b2ec4cfcd
Andrew Lazarus <lazarus@squareup.com>
2014-12-08 09:26:26 -0800

    postgresql is an optional dependency, completions

--- oneliners.rb
+++ oneliners.rb
@@ -4,9 +4,10 @@ class Oneliners < Formula
   head 'https://github.com/nerdrew/oneliners.git'
 
   depends_on 'git'
-  depends_on 'postgresql'
+  depends_on 'postgresql' => :optional
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
+    zsh_completion.install "completion/git-find-reviewers.zsh" => "_git-find-reviewers"
   end
 end
