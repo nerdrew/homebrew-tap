d63a294505aa5a005cd7915e91b696a682b14eeb
Andrew Lazarus <lazarus@squareup.com>
2015-09-08 14:42:05 -0700

    add rust beta

--- rust-beta.rb
+++ rust-beta.rb
@@ -1,13 +1,13 @@
 require 'formula'
 require 'date'
 
-class RustNightly < Formula
+class RustBeta < Formula
   homepage 'http://www.rust-lang.org/'
-  url 'https://static.rust-lang.org/dist/rust-nightly-x86_64-apple-darwin.tar.gz'
+  url 'https://static.rust-lang.org/dist/rust-beta-x86_64-apple-darwin.tar.gz'
   version Date.today.to_s
 
   conflicts_with 'rust', :because => 'same'
-  conflicts_with 'rust-beta', :because => 'same'
+  conflicts_with 'rust-nightly', :because => 'same'
 
   def install
     args = ["--prefix=#{prefix}"]
