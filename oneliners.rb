cdee113846b9010aed5c7a321fd401acf9cbc6b9
Andrew Lazarus <lazarus@squareup.com>
2017-03-08 00:09:31 -0800

    add _cargo-open

--- oneliners.rb
+++ oneliners.rb
@@ -4,10 +4,12 @@ class Oneliners < Formula
   head 'https://github.com/nerdrew/oneliners.git'
 
   depends_on 'git'
+  depends_on 'jq'
   depends_on 'postgresql' => :optional
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
+    zsh_completion.install "completion/cargo-open.zsh" => "_cargo-open"
     zsh_completion.install "completion/git-find-reviewers.zsh" => "_git-find-reviewers"
     zsh_completion.install "completion/git-fresh.zsh" => "_git-fresh"
   end
